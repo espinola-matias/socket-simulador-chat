1308ff2914fb1f039fb158ba0a90c76bb38d9768
matiasespinola <rojasmati98@gmail.com>
2025-12-02 13:19:22 -0300

    Hilo de clientes

--- servidor.py
+++ servidor.py
@@ -13,3 +13,22 @@ def mensajes_enviados(mensaje, cliente_emisor):
             except:
                 cliente.close()
                 del clientes_activos[cliente]
+
+# Hilo de clientes 
+def manejar_cliente(cliente, direccion): 
+    try:
+        nombre = cliente.recv(1024).decode()
+        clientes_activos[cliente] = nombre
+        print(f"{nombre} se ha conectado desde {direccion}")
+        mensajes_enviados(f"{nombre} se ha unido al chat", cliente)
+        cliente.send("~ Para salir del chat escribe ('salir')\n".encode())
+        cliente.send("~ Conectado al chat puedes iniciar una conversacion\n".encode())
+
+        while True:
+            mensaje = cliente.recv(1024).decode()
+            if mensaje.strip().lower() == "salir":
+                print(f"{nombre} se ha desconectado")
+                mensajes_enviados(f"{nombre} salio del chat", cliente)
+                break
+            else:
+                mensajes_enviados(f"{nombre}: {mensaje}", cliente)
