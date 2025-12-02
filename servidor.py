e9d91b812579d8db127a99f4451de02476f73391
matiasespinola <rojasmati98@gmail.com>
2025-12-02 13:18:37 -0300

    Boadcast

--- servidor.py
+++ servidor.py
@@ -3,3 +3,13 @@ import threading
 
 # diccionario de clientes activos, socket:nombre del cliente
 clientes_activos = {} 
+
+# Boadcast para el reenvio de mensajes a los demas integrantes del chat 
+def mensajes_enviados(mensaje, cliente_emisor):
+    for cliente in clientes_activos:
+        if cliente != cliente_emisor:
+            try:
+                cliente.send(mensaje.encode())
+            except:
+                cliente.close()
+                del clientes_activos[cliente]
