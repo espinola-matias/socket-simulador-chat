5e0bfcc9e5e683e343f8383923391c14aecf2004
matiasespinola <rojasmati98@gmail.com>
2025-12-02 13:24:40 -0300

    conexion de servidor

--- cliente.py
+++ cliente.py
@@ -20,3 +20,16 @@ def recibir_mensajes(cliente):
                 print("Error al recibir mensajes")
             break
     cliente.close()
+
+def conectar_servidor():
+    for intento in range(3):
+        try:
+            cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            cliente.connect(("localhost", 9999))
+            return cliente
+        except:
+            print(f"No se pudo conectar, reintentando conexion intento {intento + 1} /3")
+            time.sleep(1)
+    else:
+        print("No se logro restablecer la conexion con el servidor")
+        exit()
