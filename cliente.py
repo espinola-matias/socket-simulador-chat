ce9dfb5f5b283e08f57bca22c1cab98407a778f1
matiasespinola <rojasmati98@gmail.com>
2025-12-02 13:23:58 -0300

    Hilo de mensajes

--- cliente.py
+++ cliente.py
@@ -3,3 +3,20 @@ import threading
 import time
 
 salir_cliente = False
+
+# hilo de recibir mensajes en paralelo
+def recibir_mensajes(cliente):
+    while True:
+        try:
+            mensaje = cliente.recv(1024).decode()
+
+            if mensaje == "El servidor a cerrado la conexion":
+                print("El servidor finalizo las conexiones")
+                break
+            else:
+                print(mensaje)
+        except:
+            if not salir_cliente:
+                print("Error al recibir mensajes")
+            break
+    cliente.close()
